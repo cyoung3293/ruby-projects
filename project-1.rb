c93fc0446285ec7724381a78bc75db24bf9a1c63
Christian A. Young <c.young3293@gmail.com>
2016-05-19 02:00:43 -0400

    Added string to number conversion

--- project-1.rb
+++ project-1.rb
@@ -1,1 +1,22 @@
-#Define the alphabet to integer hash
+alpha = [*('a'..'z')]
+
+puts "Please enter a string:"
+usr_str = gets.downcase.chomp
+
+puts "Please enter the shift number:"
+usr_shift = gets.to_i
+
+usr_arr = usr_str.split("")
+
+shift_arr = usr_arr.map do |e| 
+	alpha.index(e) + usr_shift
+end
+
+shift_arr.map! do |e| 
+	if e > 26
+	e = e - 26
+	else
+	e	
+	end  
+end
+puts shift_arr
