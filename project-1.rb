5acf6887f47c3d712493dd7f94b08079b8ace209
cyoung3293 <c.young3293@gmail.com>
2016-05-22 13:59:13 -0400

    working cipher

--- project-1.rb
+++ project-1.rb
@@ -6,20 +6,30 @@ usr_str = gets.downcase.chomp
 puts "Please enter the shift number:"
 usr_shift = gets.to_i
 
-usr_arr = usr_str.split("")
+usr_words = usr_str.split(" ")
+sent_lnth = usr_words.length
+i = 0
+new_sent = []
 
-shift_arr = usr_arr.map do |e| 
-	alpha.index(e) + usr_shift
+while i < sent_lnth
+	usr_arr = usr_words[i].split("")
+	shift_arr = usr_arr.map do |e| 
+		alpha.index(e) + usr_shift
+	end
+
+	shift_arr.map! do |e| 
+		if e > 26
+		e = e - 26
+		else
+		e	
+		end  
+	end
+
+	new_arr = shift_arr.map { |e| alpha[e]  }
+
+	new_sent.push(new_arr.join(''))
+	i+=1
 end
 
-shift_arr.map! do |e| 
-	if e > 26
-	e = e - 26
-	else
-	e	
-	end  
-end
-
-new_arr = shift_arr.map { |e| alpha[e]  }
-
-puts new_arr.join("")
+new_str = new_sent.join(" ")
+puts new_str
